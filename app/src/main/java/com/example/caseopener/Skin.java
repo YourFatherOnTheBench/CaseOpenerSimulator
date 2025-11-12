e091f617f4305bb4bcd336a02930157b517d3f31
Wojtek Kopaniewski <112579904+YourFatherOnTheBench@users.noreply.github.com>
2025-11-12 13:52:58 +0100

    Update Skin.java

--- app/src/main/java/com/example/caseopener/Skin.java
+++ app/src/main/java/com/example/caseopener/Skin.java
@@ -1,14 +1,21 @@
 package com.example.caseopener;
 
 public class Skin {
+    private String imgName;
     private String name;
     private String rarity;
     private String wear;
 
-    public Skin(String name, String rarity, String wear) {
+    public Skin(String imgName, String name, String rarity, String wear) {
+
+        this.imgName = imgName;
         this.name = name;
         this.rarity = rarity;
         this.wear = wear;
+    }
+
+    public String getImgName() {
+        return imgName;
     }
 
     public String getName() {

3b5bfba51221540d881cec14519586c21072adb2
Aternasss <gilblazej@gmail.com>
2025-11-20 23:46:49 +0100

    opening skrzynek isssac
    siema siema z tej strony patryk rojo rojewski ktos to czyta? jezeli tak to chce buziaka w czolko jutro w kinie albo pograc na switchu igorkuna

--- app/src/main/java/com/example/caseopener/CaseOpening.java
+++ app/src/main/java/com/example/caseopener/CaseOpening.java
@@ -98,38 +98,47 @@ public class CaseOpening extends AppCompatActivity {
         OpenCasebtn = findViewById(R.id.OpenCase);
 
         OpenCasebtn.setOnClickListener(v -> {
-            Random rand = new Random();
 
-            String Skin_Rarity = "";
-            String rarity = "";
-            double rarityROLL = Math.random() * 100;
-            if(rarityROLL < 80.0){
-                rarity = "Consumer Grade";
-            } else if(rarityROLL < 96.0){
-                rarity = "Restricted";
-            } else if(rarityROLL < 99.0){
-                rarity = "Classified";
-            } else if(rarityROLL < 99.7){
-                rarity = "Classified";
-            }
-            String skin = "";
-            int SkinID = rand.nextInt(CaseManager.getInstance().cases.get(Case_position).skins.size());
-            skin = CaseManager.getInstance().cases.get(Case_position).skins.get(SkinID);
-            Skin RandomSkin = SkinManager.getInstance().skins_database.get(skin);
-            try {
-                JSONObject rarityJson = new JSONObject(RandomSkin.getRarity());
-                String rarityName = rarityJson.getString("name");
-
-                Skin_Rarity = rarityName;
-            } catch (JSONException e) {
-                Skin_Rarity = RandomSkin.getRarity(); // Fallback
-                e.printStackTrace();
-            }
-
-            Log.d("Skin",SkinManager.getInstance().skins_database.get(skin).name);
-            Log.d("SkinRarity",SkinManager.getInstance().skins_database.get(skin).rarity);
-            Log.d("SkinRaritygetter",Skin_Rarity);
-
+            String skin = RandomSkin_Generator(Case_position);
+            Intent OpenedCaseLoot = new Intent(CaseOpening.this, OpenedCaseLoot.class);
+            OpenedCaseLoot.putExtra("SkinID", skin);
+            OpenedCaseLoot.putExtra("CaseID", Case_position);
+            startActivity(OpenedCaseLoot);
         });
+    }
+    static String RandomSkin_Generator(int Case_position)
+    {
+        Random rand = new Random();
+
+        String Skin_Rarity = "";
+        String rarity = "";
+        double rarityROLL = Math.random() * 100;
+        if(rarityROLL < 80.0){
+            rarity = "Consumer Grade";
+        } else if(rarityROLL < 96.0){
+            rarity = "Restricted";
+        } else if(rarityROLL < 99.0){
+            rarity = "Classified";
+        } else if(rarityROLL < 99.7){
+            rarity = "Classified";
+        }
+        String skin = "";
+        int SkinID = rand.nextInt(CaseManager.getInstance().cases.get(Case_position).skins.size());
+        skin = CaseManager.getInstance().cases.get(Case_position).skins.get(SkinID);
+        Skin RandomSkin = SkinManager.getInstance().skins_database.get(skin);
+        try {
+            JSONObject rarityJson = new JSONObject(RandomSkin.getRarity());
+            String rarityName = rarityJson.getString("name");
+
+            Skin_Rarity = rarityName;
+        } catch (JSONException e) {
+            Skin_Rarity = RandomSkin.getRarity(); // Fallback
+            e.printStackTrace();
+        }
+
+        Log.d("Skin",SkinManager.getInstance().skins_database.get(skin).name);
+        Log.d("SkinRarity",SkinManager.getInstance().skins_database.get(skin).rarity);
+        Log.d("SkinRaritygetter",Skin_Rarity);
+        return skin;
     }
 }
